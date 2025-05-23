68f45df824a248a6cf41f5378cfc2c1fd4b5108e
OverMad2341 <eduardocuicas@hotmail.com>
2025-05-23 13:58:20 -0400

    v1.2

--- src/components/searchBar.vue
+++ src/components/searchBar.vue
@@ -2,6 +2,7 @@
 import { ref, watch } from "vue";
 
 const searchText = ref("");
+const isFocused = ref(false);
 const allItems = ref(["Manzana", "Banana", "Naranja", "Fresa", "Mango",
     "Uva", "Piña", "Sandía", "Melón", "Pera",
     "Kiwi", "Frambuesa", "Arándano", "Mora", "Cereza",
@@ -23,18 +24,19 @@ watch(searchText, (newText) => {
     searchResults.value = [];
   }
 });
-
 </script>
 
 <template>
-  <div
-    class="m-10 w-lg py-2 px-3 select-none"
-  >
+  <div class="m-10 w-lg py-2 px-3 select-none relative">
+    <!-- Borde animado -->
+
     <input
-      type="text"
+    type="text"
       name="search"
       v-model="searchText"
-      class="shadow shadow-gray-500 appearance-none border bg-gray-200 rounded-3xl w-full py-2 px-10 text-gray-800 leading-tight focus:shadow-outline focus:outline-2 focus:outline-offset focus:outline-blue-600"
+      @focus="isFocused = true"
+      @blur="isFocused = false"
+      class="relative shadow shadow-gray-500 appearance-none border bg-gray-200 rounded-3xl w-full py-2 px-10 text-gray-800 leading-tight focus:outline-blue-500 transition-all duration-500"
       placeholder="Buscar..."
     />
     <svg class="absolute translate-x-3 -translate-y-7.5 fill-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z">
